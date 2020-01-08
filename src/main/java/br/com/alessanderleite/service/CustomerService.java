180a952efe029c3dbbf314b2caf96462b83538d2
Alessander <alessander.logistica@gmail.com>
2020-01-07 22:07:42 -0300

    set Customer as Optional<Customer>

--- src/main/java/br/com/alessanderleite/service/CustomerService.java
+++ src/main/java/br/com/alessanderleite/service/CustomerService.java
@@ -1,12 +1,13 @@
 package br.com.alessanderleite.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.alessanderleite.model.Customer;
 
 public interface CustomerService {
 	List<Customer> getAllCustomers();
-	Customer getCustomer(Long id);
+	Optional<Customer> getCustomer(Long id);
 	Customer addCustomer(Customer customer);
 	void updateCustomer(Customer customer);
 	void deleteCustomer(Long id);
