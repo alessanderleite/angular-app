b5191b1087cbaf0bcaa9f2bb2a2c87638a24dfc6
Alessander <alessander.logistica@gmail.com>
2020-01-11 17:36:00 -0300

    change return type to Optional

--- src/main/java/br/com/alessanderleite/dao/CustomerDao.java
+++ src/main/java/br/com/alessanderleite/dao/CustomerDao.java
@@ -1,12 +1,13 @@
 package br.com.alessanderleite.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.alessanderleite.model.Customer;
 
 public interface CustomerDao {
 	List<Customer> getAllCustomers();
-	Customer getCustomer(Long id);
+	Optional<Customer> getCustomer(Long id);
 	Customer addCustomer(Customer customer);
 	void updateCustomer(Customer customer);
 	void deleteCustomer(Long id);
