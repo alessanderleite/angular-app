29d7d7ce59f91157577822216ea70e9fea16d8d9
Alessander <alessander.logistica@gmail.com>
2020-01-11 17:57:07 -0300

    implements CustomerService

---
+++ src/main/java/br/com/alessanderleite/dao/CustomerDaoImpl.java
@@ -0,0 +1,43 @@
+package br.com.alessanderleite.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.alessanderleite.model.Customer;
+import br.com.alessanderleite.repository.CustomerRepository;
+
+public class CustomerDaoImpl implements CustomerDao {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	@Override
+	public List<Customer> getAllCustomers() {
+		return customerRepository.findAll();
+	}
+
+	@Override
+	public Optional<Customer> getCustomer(Long id) {
+		return customerRepository.findById(id);
+	}
+
+	@Override
+	public Customer addCustomer(Customer customer) {
+		return customerRepository.save(customer);
+	}
+
+	@Override
+	public void updateCustomer(Customer customer) {
+		customerRepository.save(customer);
+	}
+
+	@Override
+	public void deleteCustomer(Long id) {
+		Optional<Customer> customer = this.customerRepository.findById(id);
+		if (customer.isPresent()) {
+			customerRepository.delete(customer.get());
+		}
+	}
+}
