f27b8aa3d3a2fe843708cf67c62a98e92fdd935c
Alessander <alessander.logistica@gmail.com>
2020-01-11 23:03:45 -0300

    add the method to create a new customer

--- src/main/java/br/com/alessanderleite/dao/CustomerDaoImpl.java
+++ src/main/java/br/com/alessanderleite/dao/CustomerDaoImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import br.com.alessanderleite.model.Customer;
 import br.com.alessanderleite.repository.CustomerRepository;
 
+@Repository
 public class CustomerDaoImpl implements CustomerDao {
 	
 	@Autowired
