ef3cb7a7dc89c35d7035f4ed5daba093bc8f8fa0
Alessander <alessander.logistica@gmail.com>
2020-01-12 22:01:42 -0300

    add method to load data from server

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,54 +1,74 @@
-'use strict'
+var app = angular.module('CustomerManagement', []);
 
-var app = angular.module("CustomerManagement", []);
- 
-    //Controller Part
-app.controller("CustomerController", function($scope, $http) {
-       
-        $scope.customers = [];
-        $scope.customerForm = {
-            id : -1,
-            customerName : "",
-        email : ""
-        };
-
-        $scope.submitCustomer = function() {
- 
-	        var method = "";
-	        var url = "";
-	        if ($scope.customerForm.id == -1) {
-	            //Id is absent in form data, it is create new customer operation
-	            method = "POST";
-	            url = '/addCustomer';
-	        } else {
-	            //Id is present in form data, it is edit customer operation
-	            method = "PUT";
-	            url = '/addCustomer';
-	            }
-	 
-	            $http({
-	                method : method,
-	                url : url,
-	                data : angular.toJson($scope.customerForm),
-	                headers : {
-	                    'Content-Type' : 'application/json'
-	                }
-	            }).then( _success, _error );
-        };
- 
-        function _success(response) {
-            _clearFormData()
-        }
- 
-        function _error(response) {
+//Controller Part
+app.controller('CustomerController', function($scope, $http) {
+	
+	$scope.customers = [];
+	$scope.customerForm = {
+			id : -1,
+			customerName : '',
+			email : ''
+	};
+	
+	//Load the data from server
+	_refreshCustomerData()
+	
+	$scope.submitCustomer = function() {
+		
+		var method = '';
+		var url = '';
+		
+		if ($scope.customerForm.id == -1) {
+			method = 'POST';
+			url = '/addCustomer';
+		} else {
+			method = 'PUT';
+			url = '/updateCustomer';
+		}
+		
+		$http({
+			method: method,
+			url : url,
+			data : angular.toJson($scope.customerForm),
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}).then( _success, _error );
+	};
+	
+	//Populate form fields
+    $scope.editCustomer = function(customer) {
+        $scope.customerForm.customerName = customer.customerName;
+        $scope.customerForm.email = customer.email;
+        $scope.customerForm.id = customer.id;
+    };
+	
+	//HTTP GET - get all customers
+    function _refreshCustomerData() {
+        $http({
+            method : 'GET',
+            url : 'http://localhost:8080/getAllCustomers'
+        }).then(function successCallback(response) {
+            $scope.customers = response.data;
+        }, function errorCallback(response) {
             console.log(response.statusText);
-        }
- 
-        //Clear the form
-	    function _clearFormData() {
-	        $scope.customerForm.id = -1;
-	        $scope.customerForm.customerName = "";
-	        $scope.customerForm.email = "";
-	    
-	    };
+        });
+    }
+	
+    function _success(response) {
+    	_refreshCustomerData();
+        _clearFormData();
+    }
+
+    function _error(response) {
+        console.log(response.statusText);
+    }
+
+    //Clear the form
+    function _clearFormData() {
+        $scope.customerForm.id = -1;
+        $scope.customerForm.customerName = '';
+        $scope.customerForm.email = '';
+    
+    };
 });
